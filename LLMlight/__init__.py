c77aee65d7fa128839fa84f7361c80194edbd78d
erdogant <erdogant@gmail.com>
2025-06-08 12:24:10 +0200

    update version

--- LLMlight/__init__.py
+++ LLMlight/__init__.py
@@ -12,7 +12,7 @@ import logging
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 # Setup root logger
 _logger = logging.getLogger('LLMlight')
