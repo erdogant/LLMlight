8c335890e7a6018f57b084fae43de29f22eb2681
erdogant <erdogant@gmail.com>
2025-07-21 19:46:33 +0200

    0.3.0

--- LLMlight/__init__.py
+++ LLMlight/__init__.py
@@ -10,7 +10,7 @@ from LLMlight.LLMlight import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
-__version__ = '0.3.1'
+__version__ = '0.3.0'
 
 # Setup root logger
 import logging
